c87ff383d4f53e181b701f4dccdcf137b6f8d471
EfiLeder1 <efik88@gmail.com>
2024-07-19 15:25:39 +0300

    Update db_details.php

--- db_details.php
+++ db_details.php
@@ -2,10 +2,10 @@
 session_start();
 
 // Database connection parameters
-$db_host = getenv('DB_HOST');
-$db_name = getenv('DB_NAME');
-$db_user = getenv('DB_USER');
-$db_password = getenv('DB_PASSWORD');
+$db_host = getenv('POSTGRES_HOST');
+$db_name = getenv('POSTGRES_DATABASE');
+$db_user = getenv('POSTGRES_USER');
+$db_password = getenv('POSTGRES_PASSWORD');
 
 $conn = pg_connect("host=$db_host dbname=$db_name user=$db_user password=$db_password");
 
